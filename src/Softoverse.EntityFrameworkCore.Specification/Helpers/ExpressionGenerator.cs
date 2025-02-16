cc749f364033c137a8984c82709c88a296121c81
Abir <amabirmahmud@gmail.com>
2025-02-16 19:41:09 +0600

    Create DictionaryHelper.cs

--- src/Softoverse.EntityFrameworkCore.Specification/Helpers/ExpressionGenerator.cs
+++ src/Softoverse.EntityFrameworkCore.Specification/Helpers/ExpressionGenerator.cs
@@ -69,7 +69,7 @@ public static class ExpressionGenerator<TEntity>
             object value = kvp.Value;
             if (value is JsonElement jsonElement)
             {
-                value = ConvertJsonElement(jsonElement);
+                value = jsonElement.ConvertJsonElement();
             }
             properties[lambda] = value;
         }
@@ -100,99 +100,5 @@ public static class ExpressionGenerator<TEntity>
         }
 
         return Expression.Lambda<Func<SetPropertyCalls<TEntity>, SetPropertyCalls<TEntity>>>(body, parameter);
-    }
-
-    public static IDictionary<string, object> FlattenDictionary(IDictionary<string, object> dictionary, string parentKey = "")
-    {
-        var flattened = new Dictionary<string, object>();
-
-        foreach (var kvp in dictionary)
-        {
-            string key = string.IsNullOrEmpty(parentKey) ? kvp.Key : $"{parentKey}.{kvp.Key}";
-
-            object value = kvp.Value;
-
-            // 🔹 Convert JsonElement to object if needed
-            if (value is JsonElement jsonElement)
-            {
-                value = ConvertJsonElement(jsonElement);
-            }
-
-            if (value is IDictionary<string, object> nestedDict)
-            {
-                // Recursively process nested dictionaries
-                foreach (var nestedKvp in FlattenDictionary(nestedDict, key))
-                {
-                    flattened[nestedKvp.Key] = nestedKvp.Value;
-                }
-            }
-            else
-            {
-                // Add normal key-value pairs
-                flattened[key] = value;
-            }
-        }
-
-        return flattened;
-    }
-
-    public static object ConvertJsonElement(JsonElement jsonElement)
-    {
-        object? value;
-        switch (jsonElement.ValueKind)
-        {
-            case JsonValueKind.String:
-                {
-                    var str = jsonElement.GetString();
-                    value = str is
-                    {
-                        Length: 1
-                    } ? str[0] : str;
-                    break;
-                }
-            case JsonValueKind.Number:
-                value = jsonElement.TryGetInt64(out long longValue)
-                    ? longValue
-                    : jsonElement.TryGetDouble(out double doubleValue)
-                        ? doubleValue
-                        : jsonElement.TryGetDecimal(out decimal decimalValue)
-                            ? decimalValue
-                            : jsonElement.TryGetSingle(out float floatValue)
-                                ? floatValue
-                                : jsonElement.TryGetByte(out byte byteValue)
-                                    ? byteValue
-                                    : jsonElement.TryGetSByte(out sbyte sbyteValue)
-                                        ? sbyteValue
-                                        : jsonElement.TryGetUInt16(out ushort ushortValue)
-                                            ? ushortValue
-                                            : jsonElement.TryGetInt16(out short shortValue)
-                                                ? shortValue
-                                                : jsonElement.TryGetUInt32(out uint uintValue)
-                                                    ? uintValue
-                                                    : jsonElement.TryGetInt32(out int intValue)
-                                                        ? intValue
-                                                        : jsonElement.GetDouble();
-                break;
-            case JsonValueKind.True:
-                value = true;
-                break;
-            case JsonValueKind.False:
-                value = false;
-                break;
-            case JsonValueKind.Null or JsonValueKind.Undefined:
-                value = null;
-                break;
-            case JsonValueKind.Array:
-                value = jsonElement.EnumerateArray().Select(ConvertJsonElement).ToArray();
-                break;
-            case JsonValueKind.Object:
-                value = jsonElement.EnumerateObject().ToDictionary(x => x.Name, x => ConvertJsonElement(x.Value));
-                break;
-            default:
-                value = jsonElement.ToString();
-                break;
-        }
-
-        return value!;
     }
 }
