3d1fa39d7639dedd555e4792229f3af9eee177ba
Abir <amabirmahmud@gmail.com>
2025-06-22 18:43:50 +0600

    Update Program.cs

--- tests/Benchmark/Program.cs
+++ tests/Benchmark/Program.cs
@@ -1,10 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-
-using Benchmark;
+﻿using Benchmark;
 
 using BenchmarkDotNet.Running;
 
 // BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
 
-var summary = BenchmarkRunner.Run<MyBenchmark>();
+BenchmarkRunner.Run<MyBenchmark>();
